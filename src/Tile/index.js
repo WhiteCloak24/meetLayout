158e16709d6328a2d41094a12203a407baae6910
Yash Jain <yash.jain@capanicus.com>
2024-02-12 11:38:20 +0530

    changes

--- src/Tile/index.js
+++ src/Tile/index.js
@@ -1,27 +1,15 @@
 import React, { useEffect, useRef } from "react";
 
 const Tile = ({ index, setPinned, pinned }) => {
-  const videoRef = useRef(null);
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then((stream) => {
-        videoRef.current.srcObject = stream;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
   return (
     <div
       id={`tile${index}`}
       className={`tile${index} ${pinned === index ? "main" : pinned > index ? "before-main" : "after-main"}`}
-      style={{ border: "1px solid red" }}>
+      style={{ border: "1px solid black", borderRadius: "10px" }}>
       <div>Tile&nbsp;{index}</div>
       <div style={{ cursor: "pointer" }} onClick={() => setPinned(index)}>
         {pinned === index ? "Pinned" : "Pin"}
       </div>
-      <video style={{ width: "100%", height: "100%", objectFit: "contain" }} ref={videoRef} autoPlay />
     </div>
   );
 };
